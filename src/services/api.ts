d8c36496e1312c34a105bb2c38639e00a040781a
Jaymes Costa <jaaymes@gmail.com>
2023-06-06 09:50:45 -0300

    fix: testing port

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,8 @@ import axios from 'axios'
 const isDev = process.env.NODE_ENV !== 'production'
 const PORT = process.env.PORT
 
+console.log('TESTES PORTA ==>', `http://localhost:${isDev ? 3001 : PORT}/api`)
+
 const api = axios.create({
   baseURL: `http://localhost:${isDev ? 3001 : PORT}/api`,
 })
